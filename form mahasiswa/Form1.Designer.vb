5182ccd5a78124881457d5eceab5a4b0f3ab8484
Alenaps <131380975+Alenaps@users.noreply.github.com>
2025-03-14 12:02:57 +0700

    Tugas 3

--- form mahasiswa/Form1.Designer.vb
+++ form mahasiswa/Form1.Designer.vb
@@ -34,6 +34,18 @@ Partial Class Form1
         Me.lakilaki = New System.Windows.Forms.RadioButton()
         Me.perempuan = New System.Windows.Forms.RadioButton()
         Me.jeniskelamin = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.lbNilai = New System.Windows.Forms.Label()
+        Me.lbTugas = New System.Windows.Forms.Label()
+        Me.lbUTS = New System.Windows.Forms.Label()
+        Me.lbUAS = New System.Windows.Forms.Label()
+        Me.txtTugas = New System.Windows.Forms.TextBox()
+        Me.txtUTS = New System.Windows.Forms.TextBox()
+        Me.txtUAS = New System.Windows.Forms.TextBox()
+        Me.lbGRADE = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
         Me.SuspendLayout()
         '
         'NIP
@@ -89,7 +101,7 @@ Partial Class Form1
         'Process
         '
         Me.Process.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Process.Location = New System.Drawing.Point(47, 326)
+        Me.Process.Location = New System.Drawing.Point(22, 20)
         Me.Process.Name = "Process"
         Me.Process.Size = New System.Drawing.Size(134, 37)
         Me.Process.TabIndex = 6
@@ -145,25 +157,120 @@ Partial Class Form1
         Me.jeniskelamin.TabIndex = 13
         Me.jeniskelamin.Text = "Jenis Kelamin"
         '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.lbGRADE)
+        Me.GroupBox1.Controls.Add(Me.txtUAS)
+        Me.GroupBox1.Controls.Add(Me.txtUTS)
+        Me.GroupBox1.Controls.Add(Me.txtTugas)
+        Me.GroupBox1.Controls.Add(Me.lbUAS)
+        Me.GroupBox1.Controls.Add(Me.lbUTS)
+        Me.GroupBox1.Controls.Add(Me.lbTugas)
+        Me.GroupBox1.Controls.Add(Me.lbNilai)
+        Me.GroupBox1.Location = New System.Drawing.Point(25, 31)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(807, 337)
+        Me.GroupBox1.TabIndex = 14
+        Me.GroupBox1.TabStop = False
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Process)
+        Me.GroupBox2.Location = New System.Drawing.Point(25, 383)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(807, 69)
+        Me.GroupBox2.TabIndex = 15
+        Me.GroupBox2.TabStop = False
+        '
+        'lbNilai
+        '
+        Me.lbNilai.AutoSize = True
+        Me.lbNilai.Location = New System.Drawing.Point(19, 277)
+        Me.lbNilai.Name = "lbNilai"
+        Me.lbNilai.Size = New System.Drawing.Size(38, 20)
+        Me.lbNilai.TabIndex = 16
+        Me.lbNilai.Text = "Nilai"
+        '
+        'lbTugas
+        '
+        Me.lbTugas.AutoSize = True
+        Me.lbTugas.Location = New System.Drawing.Point(188, 277)
+        Me.lbTugas.Name = "lbTugas"
+        Me.lbTugas.Size = New System.Drawing.Size(53, 20)
+        Me.lbTugas.TabIndex = 17
+        Me.lbTugas.Text = "Tugas"
+        '
+        'lbUTS
+        '
+        Me.lbUTS.AutoSize = True
+        Me.lbUTS.Location = New System.Drawing.Point(359, 278)
+        Me.lbUTS.Name = "lbUTS"
+        Me.lbUTS.Size = New System.Drawing.Size(41, 20)
+        Me.lbUTS.TabIndex = 18
+        Me.lbUTS.Text = "UTS"
+        '
+        'lbUAS
+        '
+        Me.lbUAS.AutoSize = True
+        Me.lbUAS.Location = New System.Drawing.Point(518, 276)
+        Me.lbUAS.Name = "lbUAS"
+        Me.lbUAS.Size = New System.Drawing.Size(43, 20)
+        Me.lbUAS.TabIndex = 19
+        Me.lbUAS.Text = "UAS"
+        '
+        'txtTugas
+        '
+        Me.txtTugas.Location = New System.Drawing.Point(247, 272)
+        Me.txtTugas.Name = "txtTugas"
+        Me.txtTugas.Size = New System.Drawing.Size(100, 26)
+        Me.txtTugas.TabIndex = 20
+        '
+        'txtUTS
+        '
+        Me.txtUTS.Location = New System.Drawing.Point(405, 272)
+        Me.txtUTS.Name = "txtUTS"
+        Me.txtUTS.Size = New System.Drawing.Size(100, 26)
+        Me.txtUTS.TabIndex = 21
+        '
+        'txtUAS
+        '
+        Me.txtUAS.Location = New System.Drawing.Point(567, 272)
+        Me.txtUAS.Name = "txtUAS"
+        Me.txtUAS.Size = New System.Drawing.Size(100, 26)
+        Me.txtUAS.TabIndex = 22
+        '
+        'lbGRADE
+        '
+        Me.lbGRADE.AutoSize = True
+        Me.lbGRADE.Location = New System.Drawing.Point(691, 275)
+        Me.lbGRADE.Name = "lbGRADE"
+        Me.lbGRADE.Size = New System.Drawing.Size(88, 20)
+        Me.lbGRADE.TabIndex = 23
+        Me.lbGRADE.Text = "--GRADE--"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(859, 416)
+        Me.ClientSize = New System.Drawing.Size(859, 495)
         Me.Controls.Add(Me.jeniskelamin)
         Me.Controls.Add(Me.perempuan)
         Me.Controls.Add(Me.lakilaki)
         Me.Controls.Add(Me.cmbJurusan)
         Me.Controls.Add(Me.cmbFakultas)
-        Me.Controls.Add(Me.Process)
         Me.Controls.Add(Me.Fakultas)
         Me.Controls.Add(Me.Jurusan)
         Me.Controls.Add(Me.Nama)
         Me.Controls.Add(Me.txtNama)
         Me.Controls.Add(Me.txtNip)
         Me.Controls.Add(Me.NIP)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.GroupBox2)
         Me.Name = "Form1"
         Me.Text = "oiuy"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -180,5 +287,15 @@ Partial Class Form1
     Friend WithEvents lakilaki As System.Windows.Forms.RadioButton
     Friend WithEvents perempuan As System.Windows.Forms.RadioButton
     Friend WithEvents jeniskelamin As System.Windows.Forms.Label
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents txtUAS As System.Windows.Forms.TextBox
+    Friend WithEvents txtUTS As System.Windows.Forms.TextBox
+    Friend WithEvents txtTugas As System.Windows.Forms.TextBox
+    Friend WithEvents lbUAS As System.Windows.Forms.Label
+    Friend WithEvents lbUTS As System.Windows.Forms.Label
+    Friend WithEvents lbTugas As System.Windows.Forms.Label
+    Friend WithEvents lbNilai As System.Windows.Forms.Label
+    Friend WithEvents lbGRADE As System.Windows.Forms.Label
 
 End Class
--- form mahasiswa/Form1.vb
+++ form mahasiswa/Form1.vb
@@ -31,17 +31,39 @@
 
     Private Sub Process_Click(sender As Object, e As EventArgs) Handles Process.Click
 
+        Dim nilaiTugas As Double = Double.Parse(txtTugas.Text)
+        Dim nilaiUTS As Double = Double.Parse(txtUTS.Text)
+        Dim nilaiUAS As Double = Double.Parse(txtUAS.Text)
+        Dim totalNilai As Double = (nilaiTugas * 0.3) + (nilaiUTS * 0.3) + (nilaiUAS * 0.4)
+
+        Dim grade As String
+        Select Case totalNilai
+            Case Is >= 78
+                grade = "A"
+            Case Is >= 65
+                grade = "B"
+            Case Is >= 50
+                grade = "C"
+            Case Is >= 40
+                grade = "D"
+            Case Else
+                grade = "E"
+        End Select
+
         Dim jeniskelamin As String = ""
         If lakilaki.Checked Then
             jeniskelamin = "Laki-Laki"
         Else
             jeniskelamin = "Perempuan"
         End If
+
         MessageBox.Show("Hai : " & txtNama.Text & vbCrLf &
                         "Nip : " & txtNip.Text & vbCrLf &
                         "Fakultas : " & cmbFakultas.SelectedItem & vbCrLf &
                         "Jurusan : " & cmbJurusan.SelectedItem & vbCrLf &
-                        "Jenis Kelamin : " & jeniskelamin)
+                        "Jenis Kelamin : " & jeniskelamin & vbCrLf &
+                        "Nilai Akhir: " & totalNilai.ToString("F2") & vbCrLf &
+                        "GRADE: " & grade, "Hasil")
 
     End Sub
 
@@ -54,6 +76,26 @@
     End Sub
 
     Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles lakilaki.CheckedChanged
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Label2_Click_1(sender As Object, e As EventArgs) Handles lbTugas.Click
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles lbUTS.Click
+
+    End Sub
+
+    Private Sub Label1_Click_2(sender As Object, e As EventArgs) Handles lbGRADE.Click
+
+    End Sub
+
+    Private Sub GroupBox2_Enter(sender As Object, e As EventArgs) Handles GroupBox2.Enter
 
     End Sub
 End Class
